952ee1f6d5c592d8a384b0cfc1f278d2247f637a
qyc <2638248024@qq.com>
2018-11-27 10:17:12 +0800

    two

--- app/src/main/java/soexample/umeng/com/myapplication/Two.java
+++ app/src/main/java/soexample/umeng/com/myapplication/Two.java
@@ -1,7 +1,14 @@
 package soexample.umeng.com.myapplication;
 
+import android.app.Application;
+
 /*
  * 作者：秦永聪
  *日期：2018/11/27
- * */public class Two {
+ * */public class Two  extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
 }
